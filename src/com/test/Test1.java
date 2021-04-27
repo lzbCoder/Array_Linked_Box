7b52c4cdc9b2b1af3fc3cd96313c48272ef61d70
${lzbCoder} <${1838558217@qq.com}>
2021-04-27 17:12:35 +0800

    提交

--- src/com/test/Test1.java
+++ src/com/test/Test1.java
@@ -5,7 +5,10 @@ public class Test1 {
     public static void main(String[] args) {
 
         int a = 10;
+<<<<<<< HEAD
         int b = 4;
         int c = 5;
+=======
+>>>>>>> parent of 1e022f6 (修改1)
     }
 }
